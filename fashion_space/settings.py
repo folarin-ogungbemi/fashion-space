37e6a88438ee432ab1dc3c374a2e613e127bef89
Folarin Ogungbemi <Folarine1@gmail.com>
2023-03-06 18:10:53 +0000

    setup cloudinary staticstorage

--- fashion_space/settings.py
+++ fashion_space/settings.py
@@ -46,7 +46,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    # local apps
+    # ext-apps
+    'cloudinary_storage',
+    'django.contrib.staticfiles',
+    'cloudinary',
+
+    # local-apps
     'home',
 ]
 
@@ -132,7 +137,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
